ad9f422f0435e9321793e0612eb55d39cdf57dd8
aman <a.kasa@alustudent.com>
2024-10-16 19:34:35 +0200

    humburger icon #full screen

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Toggle navbar visibility on mobile devices
 function toggleNav() {
     const navLinks = document.getElementById("navLinks");
+    const overlay = document.getElementById("fullscreenOverlay");
     if (navLinks.style.display === "flex") {
         navLinks.style.display = "none";
+        overlay.style.display = "none"; // Hide the overlay
     } else {
         navLinks.style.display = "flex";
+        overlay.style.display = "flex"; // Show the overlay
     }
 }
 
